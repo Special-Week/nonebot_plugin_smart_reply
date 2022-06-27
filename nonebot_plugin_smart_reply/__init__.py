408653df44c025bedb4f127a2a3dfca64b085a6e
Special-Week <87489040+Special-Week@users.noreply.github.com>
2022-06-27 18:26:40 +0800

    Update __init__.py

--- nonebot_plugin_smart_reply/__init__.py
+++ nonebot_plugin_smart_reply/__init__.py
@@ -5,24 +5,24 @@ from nonebot.adapters.onebot.v11 import (
     Message,
     MessageEvent,
     PokeNotifyEvent,
-    Bot
 )
 from .utils import *
 
 # 优先级99, 条件: 艾特bot就触发
 ai = on_message(rule=to_me(), priority=99,block=False)
 # 优先级1, 不会向下阻断, 条件: 戳一戳bot触发
+
 poke_ = on_notice(block=False)
 
 
 @ai.handle()
 async def _(event: MessageEvent):
     # 获取消息文本
-    msg = get_message_text(event.json())
-    if "CQ:xml" in str(event.get_message()):
-        return
+    msg = str(event.get_message())
+    # 去掉带中括号的内容(去除cq码)
+    msg = re.sub(r"\[.*?\]", "", msg)
     # 如果是光艾特bot(没消息返回)或者打招呼的话,就回复以下内容
-    if (not msg) or msg in [
+    if (not msg) or msg.isspace() or msg in [
         "你好啊",
         "你好",
         "在吗",
@@ -49,8 +49,8 @@ async def _(event: MessageEvent):
 
 
 @poke_.handle()
-async def _poke_event(bot: Bot, event: PokeNotifyEvent) -> bool:
-    # 如果戳一戳对象是自己
+async def _poke_event(event: PokeNotifyEvent):
+    # 如果事件对自己的
     if event.is_tome():
         # 随机回复poke__reply中的一条
         await poke_.send(message=f"{random.choice(poke__reply)}")
