38c37caf2e0aff8dcd59b0125b53269d9f8fb7d4
Special-Week <87489040+Special-Week@users.noreply.github.com>
2022-08-11 22:15:27 +0800

    Update __init__.py

--- nonebot_plugin_smart_reply/__init__.py
+++ nonebot_plugin_smart_reply/__init__.py
@@ -1,5 +1,6 @@
-from nonebot.plugin.on import on_message,on_notice
+from nonebot.plugin.on import on_message, on_notice, on_command
 from nonebot.rule import to_me
+from nonebot.permission import SUPERUSER
 from nonebot.adapters.onebot.v11 import (
     GroupMessageEvent,
     Message,
@@ -8,11 +9,25 @@ from nonebot.adapters.onebot.v11 import (
     MessageSegment
 )
 from .utils import *
+from loguru import logger
 
+
+# 这个值为False时, 使用的是小爱同学, True时使用的是青云客api
+api_flag = False
+# 优先级1, 向下阻断, 需要艾特bot, 智能回复api切换指令, 目前有俩api, 分别是qinyunke_api和小爱同学, 默认小爱同学
+api_switch = on_command("智障回复api切换", aliases={
+                        "ai切换", "api_switch","智能回复api切换"}, permission=SUPERUSER, rule=to_me(), block=True)
 # 优先级99, 条件: 艾特bot就触发
-ai = on_message(rule=to_me(), priority=99,block=False)
+ai = on_message(rule=to_me(), priority=99, block=False)
 # 优先级1, 不会向下阻断, 条件: 戳一戳bot触发
-poke_ = on_notice(rule=to_me(),block=False)
+poke_ = on_notice(rule=to_me(), block=False)
+
+
+@api_switch.handle()
+async def _():
+    global api_flag
+    api_flag = not api_flag
+    await api_switch.send(message=f"切换成功, 当前智能回复api为{'青云客' if api_flag else '小爱同学'}")
 
 
 @ai.handle()
@@ -40,10 +55,16 @@ async def _(event: MessageEvent):
         nickname = event.sender.nickname
     # 从字典里获取结果
     result = await get_chat_result(msg,  nickname)
-    # 如果词库没有结果，则调用qingyunke获取智能回复
+    # 如果词库没有结果，则调用api获取智能回复
     if result == None:
-        url = f"http://api.qingyunke.com/api.php?key=free&appid=0&msg={msg}"
-        message = await get_reply(url)
+        if api_flag:
+            qinyun_url = f"http://api.qingyunke.com/api.php?key=free&appid=0&msg={msg}"
+            message = await qinyun_reply(qinyun_url)
+            logger.info("来自青云客的智能回复: " + message)
+        else:
+            xiaoai_url = f"https://jintia.jintias.cn/api/xatx.php?msg={msg}"
+            message = await xiaoice_reply(xiaoai_url)
+            logger.info("来自小爱同学的智能回复: " + message)
         await ai.finish(message=message)
     await ai.finish(Message(result))
 
@@ -61,4 +82,3 @@ async def _poke_event(event: PokeNotifyEvent):
         # 随机回复poke__reply的内容
         else:
             await poke_.send(message=f"{random.choice(poke__reply)}")
-
